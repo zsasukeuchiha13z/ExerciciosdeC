c21379b253a094b984332f64ae3840be0ae26539
Carlos Alessandro Mendes Woortmann <68873700+zsasukeuchiha13z@users.noreply.github.com>
2023-05-16 18:21:59 -0300

    Att

--- Respostas em C/Exercícios C - Lista 3/08.c
+++ Respostas em C/Exercícios C - Lista 3/08.c
@@ -3,13 +3,32 @@
 int main(){
 
     int numero;
+    int maior,menor;
+    int resp1, resp2;
 
+    printf("Escreva 10 numeros inteiros: \n");
+    scanf("%d", &numero);
+    maior = numero;
+    menor = numero;
 
     for(int i = 0;i < 10;i++){
 
-        printf("Digite um numero exclusivamente, positivo: ");
-        scanf("%d", &numero);
+    scanf("%d", &numero);
+
+    if(numero > maior){
+
+        maior = numero;
 
 
+    }
+    else if(numero < menor){
 
+        menor = numero;
+
+
+    }
+
+}
+    printf("Maior: %d",maior);
+    printf("Menor: %d",menor);
 }
